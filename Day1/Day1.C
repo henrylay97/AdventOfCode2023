893f2577d25fbdc27f3bcc7fc77f4cdeb086f410
Henry Lay <h.lay@lancaster.ac.uk>
2023-12-05 17:03:23 +0000

    Complete day 1 part 2

--- Day1/Day1.C
+++ Day1/Day1.C
@@ -1,6 +1,22 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <map>
+
+const std::map<std::string, int> numberNameMap = {
+   { "one", 1 },
+   { "two", 2 },
+   { "three", 3 },
+   { "four", 4 },
+   { "five", 5 },
+   { "six", 6 },
+   { "seven", 7 },
+   { "eight", 8 },
+   { "nine", 9 }
+};
+
+void replaceFirst(const std::string &line, int firstPos, int &firstDigit);
+void replaceLast(const std::string &line, int lastPos, int &lastDigit);
 
 int main(int argc, char* argv[])
 {
@@ -21,20 +37,28 @@ int main(int argc, char* argv[])
 
       while(getline(inputFile, line))
         {
-          int firstDigit = -1;
-          int lastDigit = -1;
+          int firstDigit = -1, lastDigit = -1;
+          size_t firstPos = line.size(), lastPos = 0;
 
-          for(int i = 0; i < line.length(); ++i)
+          for(size_t i = 0; i < line.length(); ++i)
             {
               if(isdigit(line[i]))
                 {
                   const int num = line[i] - '0';
+
                   lastDigit = num;
+                  lastPos   = i;
 
                   if(firstDigit == -1)
-                    firstDigit = num;
+                    {
+                      firstDigit = num;
+                      firstPos   = i;
+                    }
                 }
             }
+
+          replaceFirst(line, firstPos, firstDigit);
+          replaceLast(line, lastPos, lastDigit);
 
           const int value = 10 * firstDigit + lastDigit;
           totalValue += value;
@@ -46,5 +70,33 @@ int main(int argc, char* argv[])
     {
       std::cout << "Failed to open file" << std::endl;
       return 1;
+    }
+}
+
+void replaceFirst(const std::string &line, int firstPos, int &firstDigit)
+{
+  for(auto const& [ name, value ] : numberNameMap)
+    {
+      size_t pos = line.find(name);
+
+      if(pos != std::string::npos && pos <= firstPos)
+        {
+          firstPos   = pos;
+          firstDigit = value;
+        }
+    }
+}
+
+void replaceLast(const std::string &line, int lastPos, int &lastDigit)
+{
+  for(auto const& [ name, value ] : numberNameMap)
+    {
+      size_t pos = line.rfind(name);
+
+      if(pos != std::string::npos && pos >= lastPos)
+        {
+          lastPos   = pos;
+          lastDigit = value;
+        }
     }
 }
